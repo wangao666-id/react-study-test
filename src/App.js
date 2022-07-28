41485c2263adf88d21a44ad335f8725aef77af78
wangao <wangao@biyouxinli.com>
2022-07-28 11:19:30 +0800

    :mod 初始化组件

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
-
+import Hello from './component/Hello'
 export default class App extends Component {
   render() {
     return (
-      
+      <div>
+         {/* 引入组件 */}
+         <Hello />
+      </div>
     )
   }
 }
